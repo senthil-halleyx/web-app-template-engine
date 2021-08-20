dd0e29c1f5391223904726241170c161ca5b4ee7
senthil-halleyx <76762064+senthil-halleyx@users.noreply.github.com>
2021-08-19 22:37:12 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,59 @@
 const express = require("express");
 const path = require("path");
-const routes = require("./index");
 const app = express();
-const port = 3000;
+const routes = require("./routes");
+const cookieSession = require("cookie-session");
+const createError = require("http-errors");
 
+const BookService = require("./services/BookService");
+const AuthorService = require("./services/AuthorService");
+const { request } = require("http");
+const { response } = require("express");
+
+const bookService = new BookService("./data/books.json");
+const authorService = new AuthorService("./data/authors.json");
+
+//app.set("trust proxy", 1);
+app.locals.siteName = "Node.js web application training"; // application level variables --> This will be available throughout the application golbally
+
+app.use((request, response, next) => {
+  try {
+    response.locals.navigation = [
+      { name: "Home", path: "/" },
+      { name: "Books", path: "/books" },
+      { name: "Authors", path: "/authors" },
+    ];
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["user_id"],
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 app.use(express.static(path.join(__dirname, "./static")));
-app.use("/", routes());
 
-app.listen(port, () => {
-  console.log("Connection established");
+app.use(
+  "/",
+  routes({ bookService: bookService, authorService: authorService })
+);
+
+app.use((request, response, next) => {
+  return next(createError(404, "File not found"));
 });
+
+app.use((err, request, response, next) => {
+  const status = err.status || 500;
+  response.locals.message = err.message;
+  response.locals.status = status;
+  response.status(status);
+  response.render("error");
+});
+app.listen(3000);
